071aa36fb417478d79db0f0e62aebefe573a699a
Elysia <a.elysia@proton.me>
2026-01-28 22:10:03 +0800

    feat: 添加PolicyKit权限配置和安装/卸载脚本

---
+++ scripts/debian-postinst.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+cp -fv /opt/apm-app-store/extras/store.spark-app.amber-pm-store.policy /usr/share/polkit-1/actions/store.spark-app.amber-pm-store.policy
---
+++ scripts/debian-postrm.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+rm -fv /usr/share/polkit-1/actions/store.spark-app.amber-pm-store.policy
